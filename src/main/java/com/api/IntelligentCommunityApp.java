d52326c15c90309aec768b66bd0096f5812f1c54
AKU001 <915083934@qq.com>
2021-01-13 11:32:50 +0800

    commit basicArchives 2021.1.13 am

--- src/main/java/com/api/IntelligentCommunityApp.java
+++ src/main/java/com/api/IntelligentCommunityApp.java
@@ -5,9 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@SpringBootApplication
 //过滤器去掉@Component，在程序启动类加上@ServletComponentScan，过滤器和urlPatterns属性均生效。
 @ServletComponentScan
+@SpringBootApplication
 @MapperScan({"com.api.manage.dao","com.api.app.dao"})
 public class IntelligentCommunityApp {
     public static void main(String[] args) {
