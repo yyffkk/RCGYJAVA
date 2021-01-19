810e43a588f9bc8cc0b2d1ce7d81cd37e665544a
AKU001 <915083934@qq.com>
2021-01-19 15:38:36 +0800

    commit basicArchives 2021.1.15 am

--- src/main/java/com/api/app/controller/butler/AppReportRepairController.java
+++ src/main/java/com/api/app/controller/butler/AppReportRepairController.java
@@ -1,6 +1,7 @@
 package com.api.app.controller.butler;
 
 import com.api.app.service.butler.AppReportRepairService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,15 @@ public class AppReportRepairController {
     @Resource
     AppReportRepairService appReportRepairService;
 
-    public Map<String,Object> list(){
+    /**
+     * app查询当前用户的报事报修信息
+     * @param pageNum 当前页数
+     * @param size 每页记录数
+     * @param id 用户主键id
+     * @return map
+     */
+    @GetMapping("/list")
+    public Map<String,Object> list(int pageNum,int size,Integer id){
         return null;
     }
 }
--- src/main/java/com/api/model/app/AppUserInfo.java
+++ src/main/java/com/api/model/app/AppUserInfo.java
@@ -1,6 +1,5 @@
 package com.api.model.app;
 
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
