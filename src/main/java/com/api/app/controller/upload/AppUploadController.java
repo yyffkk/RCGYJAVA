e565bcc8a4f5bd6de4d39ca93adf017ada53f17d
AKU001 <915083934@qq.com>
2021-01-22 11:48:47 +0800

    commit basicArchives 2021.1.21 am

--- src/main/java/com/api/app/controller/upload/AppUploadController.java
+++ src/main/java/com/api/app/controller/upload/AppUploadController.java
@@ -1,6 +1,7 @@
 package com.api.app.controller.upload;
 
 import com.api.app.service.upload.AppUploadService;
+import com.api.manage.service.system.UploadService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +18,79 @@ import java.util.Map;
 public class AppUploadController {
     @Resource
     AppUploadService appUploadService;
+    @Resource
+    UploadService uploadService;
 
+    /**
+     * 上传个人资料头像照片
+     * @param file 上传文件
+     * @return map
+     */
     @PostMapping("/appHeadSculpture")
     public Map<String,Object> appHeadSculpture(MultipartFile file){
         return appUploadService.appHeadSculpture(file);
+    }
+
+    /**
+     * 上传咨询建议照片
+     * @param file 上传文件
+     * @return map
+     */
+    @PostMapping("/uploadArticle")
+    public Map<String,Object> uploadArticle(MultipartFile file){
+        return uploadService.uploadArticle(file);
+    }
+
+    /**
+     * 上传话题照片
+     * @param file 上传文件
+     * @return map
+     */
+    @PostMapping("/uploadGambit")
+    public Map<String,Object> uploadGambit(MultipartFile file){
+        return uploadService.uploadGambit(file);
+    }
+
+
+    /**
+     * 上传固定金额分摊缴纳凭证照片
+     * @param file 上传文件
+     * @return map
+     */
+    @PostMapping("/uploadVoucher")
+    public Map<String,Object> uploadVoucher(MultipartFile file){
+        return uploadService.uploadVoucher(file);
+    }
+
+    /**
+     * 上传活动信息照片
+     * @param file 上传文件
+     * @return map
+     */
+    @PostMapping("/uploadActivity")
+    public Map<String,Object> uploadActivity(MultipartFile file){
+        return uploadService.uploadActivity(file);
+    }
+
+
+    /**
+     * 上传报事报修信息 报事报修照片
+     * @param file 上传文件
+     * @return map
+     */
+    @PostMapping("/uploadRepair")
+    public Map<String,Object> uploadRepair(MultipartFile file){
+        return uploadService.uploadRepair(file);
+    }
+
+    /**
+     * 上传业委会照片
+     * @param file 上传文件
+     * @return map
+     */
+    @PostMapping("/uploadOwnersCommittee")
+    public Map<String,Object> uploadOwnersCommittee(MultipartFile file){
+        return uploadService.uploadOwnersCommittee(file);
     }
 
 }
