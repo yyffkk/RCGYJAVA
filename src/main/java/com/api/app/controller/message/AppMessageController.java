f758d9c56123bf80cfa13903cc0076f702b82896
AKU001 <915083934@qq.com>
2021-02-09 17:22:00 +0800

    修改消息接收列表发送状态 2021.2.9 pm

--- src/main/java/com/api/app/controller/message/AppMessageController.java
+++ src/main/java/com/api/app/controller/message/AppMessageController.java
@@ -85,13 +85,13 @@ public class AppMessageController {
         return appMessageService.allRead(id);
     }
 
-    /**
-     * 【有问题】???需要改 消息列表 表，添加字段 user_delete 用户端删除
-     * 删除app消息列表
-     * @param ids 消息列表主键id数组
-     * @param request app-admin-token获取的request用户信息
-     * @return map
-     */
+//    /**
+//     * 【有问题】???需要改 消息列表 表，添加字段 user_delete 用户端删除
+//     * 删除app消息列表
+//     * @param ids 消息列表主键id数组
+//     * @param request app-admin-token获取的request用户信息
+//     * @return map
+//     */
 //    @PostMapping("/falseDelete")
 //    public Map<String,Object> falseDelete(@RequestBody VoIds ids, HttpServletRequest request){
 //        //从request获取用户id
