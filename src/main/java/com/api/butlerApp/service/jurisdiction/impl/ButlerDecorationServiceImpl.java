bccd83d0b8c2f1c434464e625fd37b64d0caeea5
AKU001 <915083934@qq.com>
2021-03-04 17:52:22 +0800

    查询装修管理信息list列表，权限冲突 2021.3.4 pm

--- src/main/java/com/api/butlerApp/service/jurisdiction/impl/ButlerDecorationServiceImpl.java
+++ src/main/java/com/api/butlerApp/service/jurisdiction/impl/ButlerDecorationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.api.butlerApp.service.jurisdiction.impl;
 
 import com.api.butlerApp.dao.jurisdiction.ButlerDecorationDao;
+import com.api.butlerApp.dao.jurisdiction.ButlerRepairDao;
 import com.api.butlerApp.service.jurisdiction.ButlerDecorationService;
 import com.api.model.butlerApp.ButlerDecorationSearch;
 import com.api.vo.butlerApp.ButlerDecorationVo;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -15,11 +17,14 @@ import java.util.Map;
 public class ButlerDecorationServiceImpl implements ButlerDecorationService {
     @Resource
     ButlerDecorationDao butlerDecorationDao;
+    @Resource
+    ButlerRepairDao butlerRepairDao;
     private static Map<String,Object> map = null;
 
 
     @Override
     public List<ButlerDecorationVo> list(ButlerDecorationSearch decorationSearch) {
+        int type = findJurisdictionByUserId(decorationSearch.getRoleId());
         List<ButlerDecorationVo> butlerDecorationVos = butlerDecorationDao.list(decorationSearch);
         if (butlerDecorationVos != null && butlerDecorationVos.size()>0){
             for (ButlerDecorationVo decorationVo : butlerDecorationVos) {
@@ -34,6 +39,9 @@ public class ButlerDecorationServiceImpl implements ButlerDecorationService {
                 }else {
                     //1.待指派【当检查跟踪人为null且装修状态为>1】查询出来的数据默认装修状态>1，所以不用判断
                     decorationVo.setOperationStatus(1);
+                    if (type == 2 || type == 3){
+                        //当为跟踪人员和其他 时，不能显示待指派状态 ？？？？？
+                    }
                 }
             }
         }
@@ -46,5 +54,25 @@ public class ButlerDecorationServiceImpl implements ButlerDecorationService {
 
 
         return map;
+    }
+
+    private int findJurisdictionByUserId(String roleIds) {
+        String[] split = roleIds.split(",");
+        if (split.length >0){
+            for (String s : split) {
+                int roleId = Integer.parseInt(s);
+                //根据角色id查询权限id集合
+                List<Integer> jurisdictionIds = butlerRepairDao.findJIdsByRoleId(roleId);
+                if (jurisdictionIds != null && jurisdictionIds.size()>0){
+                    //59.装修派工
+                    if (jurisdictionIds.contains(59)){
+                        return 1;
+                    }else if (jurisdictionIds.contains(60)){
+                        return 2;
+                    }
+                }
+            }
+        }
+        return 3;
     }
 }
