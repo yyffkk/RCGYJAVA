76adfd6470dd4063a75302fc647e74de74daf382
AKU001 <915083934@qq.com>
2021-04-15 14:30:13 +0800

    后台查询已开启的收费标准明细信息(物业管理费和日常费用),加入单价和计费单位 2021.4.15 pm

--- src/main/java/com/api/vo/chargeManagement/VoEnableTemplateDetail.java
+++ src/main/java/com/api/vo/chargeManagement/VoEnableTemplateDetail.java
@@ -1,5 +1,7 @@
 package com.api.vo.chargeManagement;
 
+import java.math.BigDecimal;
+
 /**
  * 已开启的收费标准明细信息
  */
@@ -12,12 +14,22 @@ public class VoEnableTemplateDetail {
      * 收费标准明细名称
      */
     private String name;
+    /**
+     * 单价
+     */
+    private BigDecimal unitPrice;
+    /**
+     * 计费单位（1.元/月 平方米，2.元/ 立方米，3.元/ 次）
+     */
+    private Integer type;
 
     @Override
     public String toString() {
         return "VoEnableTemplateDetail{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", type=" + type +
                 '}';
     }
 
@@ -37,11 +49,29 @@ public class VoEnableTemplateDetail {
         this.name = name;
     }
 
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
     public VoEnableTemplateDetail() {
     }
 
-    public VoEnableTemplateDetail(Integer id, String name) {
+    public VoEnableTemplateDetail(Integer id, String name, BigDecimal unitPrice, Integer type) {
         this.id = id;
         this.name = name;
+        this.unitPrice = unitPrice;
+        this.type = type;
     }
 }
