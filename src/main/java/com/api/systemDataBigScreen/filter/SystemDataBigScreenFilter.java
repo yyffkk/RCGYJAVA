c13994e7b2b88ab78d95d5d1eded30774da4037b
AKU001 <915083934@qq.com>
2021-04-26 11:20:00 +0800

    后台：第三方对外接口拦截  2021.4.26 am

--- src/main/java/com/api/systemDataBigScreen/filter/SystemDataBigScreenFilter.java
+++ src/main/java/com/api/systemDataBigScreen/filter/SystemDataBigScreenFilter.java
@@ -1,9 +1,7 @@
 package com.api.systemDataBigScreen.filter;
 
-import com.api.app.filter.SpringContextUtil;
-import com.api.app.service.login.AppLoginService;
+import com.api.app.filter.ParameterRequestWrapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -12,15 +10,14 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-//TODO 对外接口拦截有误
-//@WebFilter(urlPatterns = "/systemData/*")
+@WebFilter(urlPatterns = "/systemData/*")
 public class SystemDataBigScreenFilter implements Filter {
     ObjectMapper objectMapper = new ObjectMapper();
-    /**
-     *   第三方对外接口token值
-     */
-    @Value("${res.systemDataToken}")
-    private String SYSTEM_DATA_TOKEN;
+//    /**
+//     *   第三方对外接口token值
+//     */
+//    @Value("${res.systemDataToken}")
+//    private String SYSTEM_DATA_TOKEN;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -33,12 +30,21 @@ public class SystemDataBigScreenFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         //获取token信息
         String tokenId = req.getHeader("system-data-token");
-        if (SYSTEM_DATA_TOKEN.equals(tokenId)){
-            //通行
-            chain.doFilter(request, response);
-        }else {
+        ////如果token为''，则表示没权限
+        if (tokenId == null || "".equals(tokenId) || tokenId.isEmpty()) {
             this.respFail(response);
+            return;
         }
+        //创建一个重新包装的Request请求
+        ParameterRequestWrapper requestWrapper = new ParameterRequestWrapper(req);
+        //第三方对外接口token值{system-data-token:9627a35847e640cb92a7067557f09bda}
+        //如果不相同则提示权限认证不足
+        if (!"9627a35847e640cb92a7067557f09bda".equals(tokenId)){
+            this.respFail(response);
+            return;
+        }
+        //通行
+        chain.doFilter(requestWrapper, response);
     }
 
     /** 返回失败结果Json数据 */
