dbe281a33c6106ff71c53d8f35e42f273479e865
AKU001 <915083934@qq.com>
2021-05-08 18:35:10 +0800

    修复后台bug  2021.5.8 am

---
+++ src/main/java/com/api/manage/controller/operationManagement/SysKeyManagementController.java
@@ -0,0 +1,13 @@
+package com.api.manage.controller.operationManagement;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 钥匙管理
+ */
+@RestController
+@RequestMapping("manage/keyManagement")
+public class SysKeyManagementController {
+
+}
