8ab835f0f143a282efb3e75d9453abf33f9d38af
AKU001 <915083934@qq.com>
2021-05-19 16:45:02 +0800

    跨域 2021.5.18 am

--- src/main/java/com/api/manage/filter/CorsInterceptor.java
+++ src/main/java/com/api/manage/filter/CorsInterceptor.java
@@ -3,59 +3,29 @@ package com.api.manage.filter;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.HandlerInterceptor;
 
-import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
-public class CorsInterceptor implements Filter {
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-////        response.setHeader("Access-Control-Allow-Origin",  request.getHeader("Origin"));
-//        response.setHeader("Access-Control-Allow-Origin",  "*");
-//        response.setHeader("Access-Control-Allow-Credentials", "true");
-//        response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS");
-//        response.setHeader("Access-Control-Max-Age", "86400");
-//        response.setHeader("Access-Control-Allow-Headers", "Content-Type,X-Admin-Token");//这里“Access-Token”是我要传到后台的内容key
-//        response.setHeader("Access-Control-Expose-Headers", "*");
-//        System.out.println(request.getRequestURL());
-//        // 如果是OPTIONS则结束请求
-//        if (HttpMethod.OPTIONS.toString().equals(request.getMethod())) {
-//            response.setStatus(HttpStatus.NO_CONTENT.value());
-//            return false;
-//        }
-//
-//        return true;
-//    }
-
+public class CorsInterceptor implements HandlerInterceptor {
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse res = (HttpServletResponse) response;
-        // 设置允许Cookie
-        res.addHeader("Access-Control-Allow-Credentials", "true");
-        // 允许http://www.xxx.com域（自行设置，这里只做示例）发起跨域请求
-        res.addHeader("Access-Control-Allow-Origin", "*");
-        // 设置允许跨域请求的方法
-        res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-        // 允许跨域请求包含content-type
-        res.addHeader("Access-Control-Allow-Headers", "Content-Type,X-Admin-Token");
-        if (((HttpServletRequest) request).getMethod().equals("OPTIONS")) {
-            response.getWriter().println("ok");
-            return;
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        response.setHeader("Access-Control-Allow-Origin",  request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin",  "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "86400");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type,X-Admin-Token");//这里“Access-Token”是我要传到后台的内容key
+        response.setHeader("Access-Control-Expose-Headers", "*");
+        System.out.println(request.getRequestURL());
+        // 如果是OPTIONS则结束请求
+        if (HttpMethod.OPTIONS.toString().equals(request.getMethod())) {
+            response.setStatus(HttpStatus.NO_CONTENT.value());
+            return false;
         }
-        chain.doFilter(request, response);
-    }
 
-    @Override
-    public void destroy() {
-
+        return true;
     }
 }
