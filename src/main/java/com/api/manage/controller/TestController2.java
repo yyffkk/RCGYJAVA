01e9abce2b1d2afafd678dddd3508bc6332c3151
AKU001 <915083934@qq.com>
2021-05-28 17:04:27 +0800

    定时任务调控 2021.5.28 am

--- src/main/java/com/api/manage/controller/TestController2.java
+++ src/main/java/com/api/manage/controller/TestController2.java
@@ -2,22 +2,25 @@ package com.api.manage.controller;
 
 import com.api.util.webSocket.WebSocketService;
 //import net.sf.json.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @RestController
+@Slf4j
 @RequestMapping("manage/test2")
 public class TestController2 {
     @GetMapping("/test2")
     public Boolean test2(HttpServletRequest request) throws IOException {
-        WebSocketService ws = new WebSocketService();
+//        WebSocketService ws = new WebSocketService();
 //        JSONObject jo = new JSONObject();
 //        jo.put("message", "这是后台返回的消息！");
 //        jo.put("To","All");
 //        ws.onMessage(jo.toString());
-        ws.broadcast("这是后台返回的消息");
+//        ws.broadcast("这是后台返回的消息");
+        log.info("这个测试log日志");
         return false;
     }
 }
