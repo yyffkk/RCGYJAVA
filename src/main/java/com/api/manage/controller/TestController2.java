674c4f4dda0d8277efa4f3ce9709f99da26dfbcd
AKU001 <915083934@qq.com>
2021-05-31 18:07:40 +0800

    爬取数据 2021.5.31 am

--- src/main/java/com/api/manage/controller/TestController2.java
+++ src/main/java/com/api/manage/controller/TestController2.java
@@ -3,6 +3,8 @@ package com.api.manage.controller;
 import com.api.util.webSocket.WebSocketService;
 //import net.sf.json.JSONObject;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,6 +22,10 @@ public class TestController2 {
 //        jo.put("To","All");
 //        ws.onMessage(jo.toString());
 //        ws.broadcast("这是后台返回的消息");
+
+        Document document = Jsoup.connect("https://www.baidu.com/").get();
+        System.out.println(document);
+
         log.info("这个测试log日志");
         return false;
     }
