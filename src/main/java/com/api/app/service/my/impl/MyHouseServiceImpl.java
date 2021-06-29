a83c321dcb1af831b756c45bc5eaf7e3c8f31154
AKU001 <915083934@qq.com>
2021-06-29 18:30:10 +0800

    生成合同 2021.6.28 am

--- src/main/java/com/api/app/service/my/impl/MyHouseServiceImpl.java
+++ src/main/java/com/api/app/service/my/impl/MyHouseServiceImpl.java
@@ -432,7 +432,7 @@ public class MyHouseServiceImpl implements MyHouseService {
                 //预览合同作为底板来绘画签名
                 String src = realPath + appLeaseValidContract.getContractPreviewImgUrl();
                 String destRootPath = realPath +UPLOAD_LEASE_CONTRACT_SIGNED_PDF;
-                String dest = realPath+ "/temp" +UPLOAD_LEASE_CONTRACT_SIGNED_PDF+ descUrl+".pdf";
+                String dest = realPath+ UPLOAD_LEASE_CONTRACT_SIGNED_PDF+ descUrl+".pdf";
                 log.info("查询到可用的合同模版,路径为："+src);
                 log.info("预览合同预生成路径："+dest);
 
@@ -441,7 +441,7 @@ public class MyHouseServiceImpl implements MyHouseService {
 //                String dest = "/Users/AKU001/pdf/"+descUrl+".pdf";
 
                 ArrayList<PdfReplaceMap> pdfReplaceMaps = new ArrayList<>();
-                pdfReplaceMaps.add(new PdfReplaceMap("【$签字区】",realPath +appLeaseValidContract.getContractSignatureImgUrl()));
+                pdfReplaceMaps.add(new PdfReplaceMap("【$签字区】",realPath +"/temp" +appLeaseValidContract.getContractSignatureImgUrl()));
 //                pdfReplaceMaps.add(new PdfReplaceMap("【$签字区】","/Users/AKU001/pdf/黑色.jpeg"));
                 PdfUtils.pdfReplace(src,destRootPath,dest,pdfReplaceMaps);
             } catch (Exception e) {
