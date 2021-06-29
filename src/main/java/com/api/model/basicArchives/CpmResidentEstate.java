505597943432ee1cc7f5f1998ea426f6a0ffd891
AKU001 <915083934@qq.com>
2021-06-29 15:31:05 +0800

    住户房产关联表加入租赁主键id（只限租客）字段 2021.6.28 am

--- src/main/java/com/api/model/basicArchives/CpmResidentEstate.java
+++ src/main/java/com/api/model/basicArchives/CpmResidentEstate.java
@@ -26,6 +26,10 @@ public class CpmResidentEstate {
      * 有效时间结束（只限租客）
      */
     private Date effectiveTimeEnd;
+    /**
+     * 租赁主键id（只限租客）
+     */
+    private Integer sysLeaseId;
 
     @Override
     public String toString() {
@@ -35,6 +39,7 @@ public class CpmResidentEstate {
                 ", residentId=" + residentId +
                 ", effectiveTimeStart=" + effectiveTimeStart +
                 ", effectiveTimeEnd=" + effectiveTimeEnd +
+                ", sysLeaseId=" + sysLeaseId +
                 '}';
     }
 
@@ -78,14 +83,23 @@ public class CpmResidentEstate {
         this.effectiveTimeEnd = effectiveTimeEnd;
     }
 
+    public Integer getSysLeaseId() {
+        return sysLeaseId;
+    }
+
+    public void setSysLeaseId(Integer sysLeaseId) {
+        this.sysLeaseId = sysLeaseId;
+    }
+
     public CpmResidentEstate() {
     }
 
-    public CpmResidentEstate(Integer id, Integer buildingUnitEstateId, Integer residentId, Date effectiveTimeStart, Date effectiveTimeEnd) {
+    public CpmResidentEstate(Integer id, Integer buildingUnitEstateId, Integer residentId, Date effectiveTimeStart, Date effectiveTimeEnd, Integer sysLeaseId) {
         this.id = id;
         this.buildingUnitEstateId = buildingUnitEstateId;
         this.residentId = residentId;
         this.effectiveTimeStart = effectiveTimeStart;
         this.effectiveTimeEnd = effectiveTimeEnd;
+        this.sysLeaseId = sysLeaseId;
     }
 }
