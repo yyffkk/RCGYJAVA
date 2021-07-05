21a35302a4ac7335f6efe6bb6c72c7fd406f0457
AKU001 <915083934@qq.com>
2021-07-05 18:10:15 +0800

    查询所有的物料盘点管理信息  2021.7.5 am

---
+++ src/main/java/com/api/model/operationManagement/SysMaterialInventory.java
@@ -0,0 +1,18 @@
+package com.api.model.operationManagement;
+
+import java.util.Date;
+
+/**
+ * 物资盘点管理
+ */
+public class SysMaterialInventory {
+//    /**
+//     *
+//     */
+//    private Integer id;
+//    private String periodTime;
+//    private Date inventoryDateStart;
+//    private Date inventoryDateEnd;
+//    private Integer createId;
+//    private Date createDate;
+}
---
+++ src/main/java/com/api/model/operationManagement/SysMaterialInventoryDetail.java
@@ -0,0 +1,4 @@
+package com.api.model.operationManagement;
+
+public class SysMaterialInventoryDetail {
+}
