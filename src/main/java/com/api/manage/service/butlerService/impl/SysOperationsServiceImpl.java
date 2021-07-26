3a0ef8b07a5282a66f3d7fd90757f78ab07494d7
AKU001 <915083934@qq.com>
2021-07-26 09:50:02 +0800

    添加运维管理信息  2021.7.26 am

--- src/main/java/com/api/manage/service/butlerService/impl/SysOperationsServiceImpl.java
+++ src/main/java/com/api/manage/service/butlerService/impl/SysOperationsServiceImpl.java
@@ -2,12 +2,17 @@ package com.api.manage.service.butlerService.impl;
 
 import com.api.manage.dao.butlerService.SysOperationsDao;
 import com.api.manage.service.butlerService.SysOperationsService;
+import com.api.model.businessManagement.SysUser;
 import com.api.model.butlerService.SearchOperations;
 import com.api.model.butlerService.SysOperations;
+import com.api.util.IdWorker;
 import com.api.vo.butlerService.VoOperations;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,6 +31,14 @@ public class SysOperationsServiceImpl implements SysOperationsService {
     @Override
     public Map<String, Object> insert(SysOperations sysOperations) {
         map = new HashMap<>();
+
+        //获取登录用户信息
+        Subject subject = SecurityUtils.getSubject();
+        SysUser sysUser = (SysUser) subject.getPrincipal();
+
+        sysOperations.setCreateId(sysUser.getId());//填入创建人id
+        sysOperations.setCreateDate(new Date());//填入创建时间
+        sysOperations.setCode(String.valueOf(new IdWorker(1, 1, 1).nextId()));//填入记录编号
 
         int insert = sysOperationsDao.insert(sysOperations);
         if (insert > 0){
