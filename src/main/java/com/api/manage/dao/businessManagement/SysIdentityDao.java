6642635014f6e001dbb7dd3a51dab5b605120615
kanade <3136520963@qq.com>
2021-08-24 16:50:26 +0800

    身份(职业,岗位)管理

--- src/main/java/com/api/manage/dao/businessManagement/SysIdentityDao.java
+++ src/main/java/com/api/manage/dao/businessManagement/SysIdentityDao.java
@@ -10,4 +10,39 @@ public interface SysIdentityDao {
      * @return 所有的身份（职业）信息
      */
     List<SysIdentity> listAll();
+
+    /**
+     * 添加身份（职业,岗位信息）
+     * @param sysIdentity 身份（职业,岗位）
+     * @return 影响行数
+     */
+    int insert(SysIdentity sysIdentity);
+
+    /**
+     * 根据身份主键id查询身份信息
+     * @param id 身份主键id
+     * @return 身份信息
+     */
+    SysIdentity findById(Integer id);
+
+    /**
+     * 修改身份信息
+     * @param sysIdentity 身份信息
+     * @return 影响行数
+     */
+    int update(SysIdentity sysIdentity);
+
+    /**
+     * 根据身份主键id查询是否有角色拥有该身份
+     * @param positionId 身份主键id
+     * @return 拥有人数
+     */
+    int findUserByPositionId(int positionId);
+
+    /**
+     * 根据身份主键id删除身份信息
+     * @param id 身份主键id
+     * @return 身份信息
+     */
+    int delete(int id);
 }
--- src/main/java/com/api/manage/service/businessManagement/SysIdentityService.java
+++ src/main/java/com/api/manage/service/businessManagement/SysIdentityService.java
@@ -1,7 +1,17 @@
 package com.api.manage.service.businessManagement;
 
+import com.api.model.system.SysIdentity;
+
 import java.util.Map;
 
 public interface SysIdentityService {
     Map<String, Object> listAll();
+
+    Map<String, Object> insert(SysIdentity sysIdentity);
+
+    Map<String, Object> findById(Integer id);
+
+    Map<String, Object> update(SysIdentity sysIdentity);
+
+    Map<String, Object> delete(int[] ids);
 }
