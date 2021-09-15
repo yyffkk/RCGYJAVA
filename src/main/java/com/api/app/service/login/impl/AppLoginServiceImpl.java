4f897009447d7f8a2bdc4273947702f90eff3eb4
kanade <3136520963@qq.com>
2021-09-15 16:12:20 +0800

    验证手机号格式

--- src/main/java/com/api/app/service/login/impl/AppLoginServiceImpl.java
+++ src/main/java/com/api/app/service/login/impl/AppLoginServiceImpl.java
@@ -47,7 +47,7 @@ public class AppLoginServiceImpl implements AppLoginService {
         final String MOBILE = userCode.getTel();
 
         //验证手机号格式
-        Pattern p=Pattern.compile("^1(3[0-9]|4[5,7]|5[0,1,2,3,5,6,7,8,9]|6[2,5,6,7]|7[0,1,7,8]|8[0-9]|9[1,8,9])\\d{8}$");
+        Pattern p=Pattern.compile("^1[3456789]\\d{9}$");
         Matcher m=p.matcher(MOBILE);
         boolean matches = m.matches();
         if (!matches){
