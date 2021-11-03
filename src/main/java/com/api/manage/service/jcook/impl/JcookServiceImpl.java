1db054ccd7a2bbb7a11e081503a69b167225e5a5
kanade <3136520963@qq.com>
2021-11-03 16:55:08 +0800

    jcook配置

--- src/main/java/com/api/manage/service/jcook/impl/JcookServiceImpl.java
+++ src/main/java/com/api/manage/service/jcook/impl/JcookServiceImpl.java
@@ -2,6 +2,10 @@ package com.api.manage.service.jcook.impl;
 
 import com.api.manage.dao.jcook.JcookDao;
 import com.api.manage.service.jcook.JcookService;
+import org.example.api.JcookSDK;
+import org.example.api.model.SkuListRequest;
+import org.example.api.model.SkuListResponse;
+import org.example.api.utils.result.Result;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -14,6 +18,14 @@ public class JcookServiceImpl implements JcookService {
 
     @Override
     public Map<String, Object> updateJcookShop() {
+
+        SkuListRequest skuListRequest = new SkuListRequest();
+        skuListRequest.setPage(1);
+        skuListRequest.setPageSize(10);
+        JcookSDK jcookSDK = new JcookSDK("b7964889cedfdf429bfc7fae0001ff46", "7b8ef75532ca0d664c7f9fe3c174eed9", 1010256);
+        Result<SkuListResponse> skuListResponse = jcookSDK.skuList(skuListRequest);
+        System.out.println(skuListResponse);
+
         return null;
     }
 }
